447aa48492fc663ca40b159fbfd14b64bfb0d56c
micha <mboczon01@gmail.com>
2025-11-09 09:47:00 +0100

    lab2

---
+++ src/main/java/pl/wsb/fitnesstracker/healthmetrics/healthMetrics.java
@@ -0,0 +1,51 @@
+package pl.wsb.fitnesstracker.healthmetrics.api;
+
+import jakarta.annotation.Nullable;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import pl.wsb.fitnesstracker.user.api.User;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "health_metrics")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@ToString(exclude = "user")
+public class healthMetrics {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Nullable
+    private Long id;
+
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @Column(nullable = false)
+    private LocalDate date;
+
+    @Column(nullable = false)
+    private double weight;
+
+    @Column(nullable = false)
+    private double height;
+
+    @Column(name = "heart_rate")
+    private Integer heartRate;
+
+    public healthMetrics(LocalDate date, double weight, double height, Integer heartRate) {
+        this.date = date;
+        this.weight = weight;
+        this.height = height;
+        this.heartRate = heartRate;
+    }
+
+    public void assignUser(User user) {
+        this.user = user;
+    }
+}
