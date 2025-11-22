22241f32ec68dc7d003a90722bfdaf9e77b72c22
boczon <mboczon01@gmail.com>
2025-11-22 15:50:29 +0100

    Added event repo lab 3

---
+++ src/main/java/pl/wsb/fitnesstracker/event/EventRepo.java
@@ -0,0 +1,30 @@
+package pl.wsb.fitnesstracker.event;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import jakarta.persistence.TypedQuery;
+import org.springframework.stereotype.Repository;
+
+
+import java.util.List;
+
+@Repository
+public class EventRepo {
+
+    @PersistenceContext
+    private final EntityManager entityManager;
+
+    public EventRepo(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
+
+    public List<Event> findByName(String name) {
+        String jpql = "SELECT * FROM Event WHERE Event.name = :eventName";
+
+        TypedQuery<Event> query = entityManager.createQuery(jpql, Event.class);
+
+        query.setParameter("eventName", name);
+
+        return query.getResultList();
+    }
+}
