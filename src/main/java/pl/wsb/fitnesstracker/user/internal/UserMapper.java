3a93ebf7aa81101aa041a9ff6ce0143d0f63a94b
mboczon01@gmail.com <mboczon01@gmail.com>
2025-12-06 15:50:26 +0100

    zad 1

--- src/main/java/pl/wsb/fitnesstracker/user/internal/UserMapper.java
+++ src/main/java/pl/wsb/fitnesstracker/user/internal/UserMapper.java
@@ -8,10 +8,51 @@ import pl.wsb.fitnesstracker.user.api.UserDto;
 class UserMapper {
 
     UserDto toDto(User user) {
-        return new UserDto(user.getId(),
+        return new UserDto(
+                user.getId(),
                 user.getFirstName(),
                 user.getLastName(),
                 user.getBirthdate(),
-                user.getEmail());
+                user.getEmail()
+        );
+    }
+
+    User toEntity(UserDto userDto) {
+        return new User(
+                userDto.firstName(),
+                userDto.lastName(),
+                userDto.birthdate(),
+                userDto.email()
+        );
+    }
+
+    UserSimpleDto toSimpleDto(User user) {
+        return new UserSimpleDto(
+                user.getId(),
+                user.getFirstName(),
+                user.getLastName()
+        );
+    }
+
+    UserEmailSimpleDto toEmailSimpleDto(User user) {
+        return new UserEmailSimpleDto(
+                user.getId(),
+                user.getEmail()
+        );
+    }
+
+    void updateUserFromDto(User user, UserDto userDto) {
+        if (userDto.firstName() != null) {
+            user.setFirstName(userDto.firstName());
+        }
+        if (userDto.lastName() != null) {
+            user.setLastName(userDto.lastName());
+        }
+        if (userDto.email() != null) {
+            user.setEmail(userDto.email());
+        }
+        if (userDto.birthdate() != null) {
+            user.setBirthdate(userDto.birthdate());
+        }
     }
 }
